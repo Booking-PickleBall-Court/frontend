a49781834bae3c2281b674f2244ac1628a5758a7
phuclh <phuclh121003@gmail.com>
2025-06-10 22:04:07 +0700

    Update API_URL in api.js to remove redundant path segment

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://picklenet-api.onrender.com/api";
+const API_URL = "https://picklenet-api.onrender.com";
 
 const api = axios.create({
   baseURL: API_URL,
