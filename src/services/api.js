068da42a90447325be2d02070b48765b120cdf10
phuclh <phuclh121003@gmail.com>
2025-06-13 13:48:00 +0700

    Update API URL to point to the correct backend endpoint

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://picklenet-api.onrender.com/api";
+const API_URL = "https://backend-eej5.onrender.com/api";
 
 const api = axios.create({
   baseURL: API_URL,
